5b49ef2fb5a682dd8fcade48599c75dcdfd337c9
Dogboy21 <Dogboy2103@gmail.com>
2022-12-01 15:13:17 +0100

    style: compact lambdas a bit

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -7,22 +7,16 @@ fn main() -> NullResult {
 
     let elves: Vec<_> = input.split("\n\n").collect();
 
-    let mut elf_calories_sum: Vec<u32> = elves.iter().map(|elf_block| {
-        return elf_block.lines().into_iter().map(|calorie_item| match calorie_item.parse::<u32>() {
-                Ok(n) => n,
-                Err(_) => 0,
-        }).sum();
-    }).collect();
+    let mut elf_calories_sum: Vec<u32> = elves.iter()
+        .map(|elf_block| elf_block.lines().into_iter().map(|calorie_item| calorie_item.parse::<u32>().unwrap_or(0)).sum())
+        .collect();
 
     elf_calories_sum.sort_by(|a, b| b.cmp(a));
 
     match exec_part {
-        ExecutionPart::Part1 => {
-            println!("The Elf carrying the most Calories carries {} Calories.", elf_calories_sum.get(0).unwrap_or(&0));
-        },
+        ExecutionPart::Part1 => println!("The Elf carrying the most Calories carries {} Calories.", elf_calories_sum.get(0).unwrap_or(&0)),
         ExecutionPart::Part2 => {
             let sum: u32 = elf_calories_sum.iter().take(3).sum();
-
             println!("The top three Elves carry {} Calories in total", sum)
         },
     }
