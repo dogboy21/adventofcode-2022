abec6d00b6efc03f6daea2f4f3462d056e52a6f3
Dogboy21 <dogboy2103@gmail.com>
2022-12-05 21:02:37 +0100

    feat: add scaffold command to create new day from template

---
+++ src/bin/scaffold.rs
@@ -0,0 +1,127 @@
+use std::{
+    fs::{File, OpenOptions},
+    io::Write,
+    process,
+};
+
+use aoc_common::get_aoc_input;
+
+use chrono::Datelike;
+
+use clap::Parser;
+
+const MODULE_TEMPLATE: &str = r###"use aoc_common::*;
+
+const BIN: &str = env!("CARGO_BIN_NAME");
+
+fn main() -> NullResult {
+    let (input, exec_part) = get_input_from_args(BIN)?;
+
+    match exec_part {
+        ExecutionPart::Part1 => {},
+        ExecutionPart::Part2 => {},
+    }
+
+    return Ok(());
+}
+"###;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    day: u8,
+
+    #[arg(short, long)]
+    year: Option<i32>,
+}
+
+fn parse_args() -> Args {
+    return Args::parse();
+}
+
+fn safe_create_file(path: &str) -> Result<File, std::io::Error> {
+    OpenOptions::new().write(true).create_new(true).open(path)
+}
+
+fn create_file(path: &str) -> Result<File, std::io::Error> {
+    OpenOptions::new().write(true).create(true).open(path)
+}
+
+fn main() {
+    let args = parse_args();
+
+    let inputs_dir = format!("inputs/{}", args.day);
+    let input_path = format!("inputs/{}/input.txt", args.day);
+    let example_path = format!("inputs/{}/example_0.txt", args.day);
+    let module_path = format!("src/bin/day{}.rs", args.day);
+
+    let mut file = match safe_create_file(&module_path) {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Failed to create module file: {}", e);
+            process::exit(1);
+        }
+    };
+
+    match file.write_all(MODULE_TEMPLATE.as_bytes()) {
+        Ok(_) => {
+            println!("Created module file \"{}\"", &module_path);
+        }
+        Err(e) => {
+            eprintln!("Failed to write module contents: {}", e);
+            process::exit(1);
+        }
+    }
+
+    match std::fs::create_dir(&inputs_dir) {
+        Ok(_) => {},
+        Err(e) => {
+            eprintln!("Failed to create inputs dir: \"{}\"", &inputs_dir);
+            process::exit(1);
+        }
+    }
+
+    match create_file(&input_path) {
+        Ok(_) => {
+            println!("Created empty input file \"{}\"", &input_path);
+        }
+        Err(e) => {
+            eprintln!("Failed to create input file: {}", e);
+            process::exit(1);
+        }
+    }
+
+    match create_file(&example_path) {
+        Ok(_) => {
+            println!("Created empty example file \"{}\"", &example_path);
+        }
+        Err(e) => {
+            eprintln!("Failed to create example file: {}", e);
+            process::exit(1);
+        }
+    }
+
+    match get_aoc_input(args.year.unwrap_or(chrono::Utc::now().year()), args.day) {
+        Ok(input) => {
+            match std::fs::write(&input_path, input) {
+                Ok(_) => {
+                    println!("Successfully wrote AoC input to file \"{}\"", &input_path)
+                },
+                Err(e) => {
+                    eprintln!("Failed to write AoC input file: {}", e);
+                    process::exit(1);
+                }
+            }
+        },
+        Err(e) => {
+            eprintln!("Failed to get AoC input: {}", e);
+            process::exit(1);
+        }
+    }
+
+    println!("---");
+    println!(
+        "🎄 Type `cargo solve day{}` to run your solution.",
+        &args.day
+    );
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod cli;
 
-type GenericResult<T> = Result<T, Box<dyn std::error::Error>>;
+use std::{io, fs, error};
+
+type GenericResult<T> = Result<T, Box<dyn error::Error>>;
 pub type NullResult = GenericResult<()>;
 
 pub enum ExecutionPart {
@@ -18,20 +20,20 @@ impl From<bool> for ExecutionPart {
     }
 }
 
-fn get_inputs(day: u8, example_input: Option<u8>) -> std::io::Result<String> {
+fn get_inputs(day: u8, example_input: Option<u8>) -> io::Result<String> {
     let file_path = match example_input {
         Some(n) => format!("./inputs/{day}/example_{n}.txt"),
         None => format!("./inputs/{day}/input.txt"),
     };
 
-    return std::fs::read_to_string(file_path);
+    return fs::read_to_string(file_path);
 }
 
-pub fn get_input_from_args(bin_name: &str) -> std::io::Result<(String, ExecutionPart)> {
+pub fn get_input_from_args(bin_name: &str) -> io::Result<(String, ExecutionPart)> {
     let day_str = &bin_name[3..];
     let day = match day_str.parse::<u8>() {
         Ok(n) => n,
-        Err(_) => return Err(std::io::Error::new(std::io::ErrorKind::Other, "failed to parse day from binary name")),
+        Err(_) => return Err(io::Error::new(io::ErrorKind::Other, "failed to parse day from binary name")),
     };
 
     let args = cli::parse_args();
@@ -39,4 +41,29 @@ pub fn get_input_from_args(bin_name: &str) -> std::io::Result<(String, Execution
         Ok(input) => Ok((input, ExecutionPart::from(args.part2))),
         Err(err) => Err(err)
     };
+}
+
+fn get_aoc_token() -> io::Result<String> {
+    match dirs::home_dir() {
+        Some(home_dir) => match fs::read_to_string(home_dir.join(".aoc_auth_token")) {
+            Ok(token) => Ok(token.trim().into()),
+            Err(err) => Err(err),
+        },
+        None => Err(io::Error::new(io::ErrorKind::NotFound, "home directory not found")),
+    }
+}
+
+pub fn get_aoc_input(year: i32, day: u8) -> GenericResult<String> {
+    let aoc_token = get_aoc_token()?;
+
+    let client = reqwest::blocking::Client::builder()
+        .user_agent("github.com/dogboy21/adventofcode-2022 by info@dogboy.xyz")
+        .build()?;
+
+    let resp = client.get(format!("https://adventofcode.com/{year}/day/{day}/input"))
+        .header("Cookie", format!("session={aoc_token}").as_str())
+        .send()?;
+
+    let text_content = resp.text()?;
+    return Ok(text_content);
 }
